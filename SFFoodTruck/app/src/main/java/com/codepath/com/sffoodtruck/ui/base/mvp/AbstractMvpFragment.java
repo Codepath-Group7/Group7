bfb3d37aa9630a1871c44a715420084ede66b7e1
robl2e <robl2e@users.noreply.github.com>
2017-10-13 23:59:50 -0700

    Add support to apply MVP.

---
+++ SFFoodTruck/app/src/main/java/com/codepath/com/sffoodtruck/ui/base/mvp/AbstractMvpFragment.java
@@ -0,0 +1,12 @@
+package com.codepath.com.sffoodtruck.ui.base.mvp;
+
+
+import com.hannesdorfmann.mosby3.mvp.MvpFragment;
+
+/**
+ * Created by robl2e on 10/9/17.
+ */
+
+public abstract class AbstractMvpFragment<V extends MvpBaseView, P extends MvpBasePresenter<V>>
+        extends MvpFragment<V, P> {
+}
